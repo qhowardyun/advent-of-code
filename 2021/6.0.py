c400e5d90832d70f4129a0f22856b943717d2cca
Howard Yun <haoy2001@gmail.com>
2021-12-06 00:09:29 -0500

    2021 Day 6

---
+++ 2021/6.0.py
@@ -0,0 +1,30 @@
+from aocd import get_data, submit
+
+data = get_data(day=6, year=2021)
+lines = data.splitlines()
+
+# lines = ["3,4,3,1,2"]
+
+c = {i: 0 for i in range(9)}
+
+for num in lines[0].split(","):
+    c[int(num)] += 1
+
+for _ in range(80):
+    spawned = c[0]
+    c[0] = c[1]
+    c[1] = c[2]
+    c[2] = c[3]
+    c[3] = c[4]
+    c[4] = c[5]
+    c[5] = c[6]
+    c[6] = c[7] + spawned
+    c[7] = c[8]
+    c[8] = spawned
+
+ans = 0
+for k, v in c.items():
+    ans += v
+
+print(ans)
+submit(ans, part="a", day=6, year=2021, reopen=False)
