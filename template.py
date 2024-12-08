8e84ae813469f91061f921b77e1bfd63682caee5
Howard Yun <haoy2001@gmail.com>
2024-12-07 23:53:35 -0500

    2024 template

--- template.py
+++ template.py
@@ -1,12 +1,22 @@
-from typing import Counter
 from aocd import get_data, submit
 from collections import defaultdict, Counter
 from functools import lru_cache
+from re import search, findall
 
 day = 1
-data = get_data(day=day, year=2021)
+data = get_data(day=day, year=2024)
+
 lines = data.splitlines()
+lines = [int(x) for x in lines]
 ans = 0
 
+for line in lines:
+    pass
+
+
+
 print(ans)
-# submit(ans, part="a", day=day, year=2021, reopen=False)
+assert ans != 0
+input()
+submit(ans, part="a", day=day, year=2024, reopen=False)
+# submit(ans, part="b", day=day, year=2024, reopen=False)
